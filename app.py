3ca010f0e6b425b3e2aafdef7bddebd6f6cb32aa
Lương Phúc Hậu <92417291+kienvan69cm@users.noreply.github.com>
2026-03-12 15:16:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ VOICES_FILE = APP_DIR / "voices.json"
 SETTINGS_FILE = APP_DIR / "settings.json"
 SENTENCES_META_FILE = OUTPUT_DIR / "sentences.json"
 
-PROXY_API_URL = "http://113.161.55.198:8787/v1/tts"
+PROXY_API_URL = "https://unendowed-jacquelyn-guilefully.ngrok-free.dev/v1/tts"
 
 DEFAULT_VOICES = {
     "Mặc định": "hruBcESGYx2AUWRppNacCd"
@@ -932,3 +932,4 @@ with tab2:
                     use_container_width=False,
 
                 )
+
